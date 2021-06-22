5961eb4130974e49c6becd9aeb8f2d0bd810f6b1
SchillingW <schillingw24@bolles.org>
2021-06-22 16:15:32 -0400

    ReOriginal Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotDevice.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+
+// high level class encompassing any module or hardware group on the bot
+public class RobotDevice {
+
+    public RobotDevice[] devices;
+}
